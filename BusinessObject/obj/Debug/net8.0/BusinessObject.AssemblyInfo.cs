659f83fe4a4c077d69c90be023023088639a8bb0
Dolakien <ngomeo098@gmail.com>
2024-11-04 23:02:41 +0700

    Final Done!!!

--- BusinessObject/obj/Debug/net8.0/BusinessObject.AssemblyInfo.cs
+++ BusinessObject/obj/Debug/net8.0/BusinessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fde1a33b322e96216f6cf91bd8b3339f45f0630b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7a64ade5e79a4a641209851ea322fe3a6a8e3b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
